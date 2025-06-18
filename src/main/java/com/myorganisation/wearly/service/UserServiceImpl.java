42c1e5235820b7d2739b2b95265f13615a047a11
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-06-18 12:22:56 +0530

    Override all methods in UserServiceImpl

--- src/main/java/com/myorganisation/wearly/service/UserServiceImpl.java
+++ src/main/java/com/myorganisation/wearly/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.myorganisation.wearly.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -53,16 +54,61 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserResponseDTO> getAllUsers() {
-        return null;
+        //Get all users from database (in User)
+        List<User> userList = userRepository.findAll();
+
+        //Create a list of UserResponseDTO
+        List<UserResponseDTO> userResponseDTOList = new LinkedList<>();
+
+        //traversal on a list of User
+        for(User user : userList) {
+            //conversion of User to UserResponseDTO
+            UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+            userResponseDTO.setId(user.getId());
+            userResponseDTO.setName(user.getName());
+            userResponseDTO.setGender(user.getGender());
+            userResponseDTO.setEmail(user.getEmail());
+            userResponseDTO.setPhone(user.getPhone());
+
+            //Inserting UserResponseDTO to list of UserResponseDTO
+            userResponseDTOList.add(userResponseDTO);
+        }
+
+        return userResponseDTOList;
     }
 
     @Override
     public UserResponseDTO updateUser(Long id, UserRequestDTO userRequestDTO) {
-        return null;
+        User user = userRepository.findById(id).orElse(null);
+
+        if(user == null) {
+            System.out.println("User doesn't exist!");
+        } else {
+            user.setName(userRequestDTO.getName());
+            user.setGender(userRequestDTO.getGender());
+            user.setEmail(userRequestDTO.getEmail());
+            user.setPhone(userRequestDTO.getPhone());
+            user.setPassword(userRequestDTO.getPassword());
+
+            userRepository.save(user);
+        }
+
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+        userResponseDTO.setId(user.getId());
+        userResponseDTO.setName(user.getName());
+        userResponseDTO.setGender(user.getGender());
+        userResponseDTO.setEmail(user.getEmail());
+        userResponseDTO.setPhone(user.getPhone());
+
+        return userResponseDTO;
     }
 
     @Override
     public String removeUser(Long id) {
-        return "";
+        String name = userRepository.findById(id).orElse(null).getName();
+        userRepository.deleteById(id);
+        return "User name: " + name + "( " + id + ") has been removed successfully!";
     }
 }
