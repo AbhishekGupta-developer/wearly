61a4fe3bfb034d01488a0294d2f5314bdad335b7
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-15 12:30:15 +0530

    Comment test cron job

--- src/main/java/com/myorganisation/wearly/jobs/CronJobs.java
+++ src/main/java/com/myorganisation/wearly/jobs/CronJobs.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CronJobs {
-    private int flag = 1;
-
-    //CRON job runs at every five seconds
-    @Scheduled(cron = "*/5 * * * * ?")
-    public void jobRunsAtEveryMinute() {
-        System.out.println("Hello, " + flag++);
-    }
+//    private int flag = 1;
+//
+//    //CRON job runs at every five seconds
+//    @Scheduled(cron = "*/5 * * * * ?")
+//    public void jobRunsAtEveryMinute() {
+//        System.out.println("Hello, " + flag++);
+//    }
 }
